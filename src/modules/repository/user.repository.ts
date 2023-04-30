c2e3b3c8bc85e67c2db4919e684eb335905dc987
Leon68-dev <leon-dev@ukr.net>
2023-04-30 17:22:03 +0300

    add procedures call

--- src/modules/repository/user.repository.ts
+++ src/modules/repository/user.repository.ts
@@ -2,18 +2,30 @@ import poolConnections from "../config/database.js"
 
 export const createUserRpst = async (name: string, surname: string):Promise<any> => {
     try{
-        const newUser = await poolConnections.query(`INSERT INTO person(name, surname) values($1, $2) RETURNING *`, [name, surname]);
+        //const newUser = await poolConnections.query(`INSERT INTO person(name, surname) values($1, $2) RETURNING *`, [name, surname]);
+        let res_is = 0;
+        const newUser = await poolConnections.query(`CALL public.insert_person($1, $2, $3)`, [name, surname, res_is]);
+        //console.log('newUser', newUser);
+        console.log('newUser.rows[0]', newUser.rows[0]);
         return newUser?.rows[0];
     } catch(e) {
+        await poolConnections.query(`ROLLBACK;`);
+        console.error('insert_person', e);
         return null;
     }
 }
 
 export const getUsersRpst = async ():Promise<any[] | null> => {
     try {
-        const users = await poolConnections.query(`SELECT * FROM person`);
+        //const users = await poolConnections.query(`SELECT * FROM person`);
+        await poolConnections.query(`BEGIN;`);
+        await poolConnections.query(`CALL public.get_person('result')`);
+        const users = await poolConnections.query(`FETCH ALL IN "result";`);
+        await poolConnections.query(`COMMIT;`);
         return users.rows;
     } catch(e) {
+        await poolConnections.query(`ROLLBACK;`);
+        console.error('error', e);
         return null;
     }
 }
@@ -47,17 +59,3 @@ export const deleteUserRpst = async (id: number):Promise<any> => {
         return null;
     }
 }
-
-export const getUsersProcedureRpst = async ():Promise<any[] | null> => {
-    try {
-        await poolConnections.query(`BEGIN;`);
-        await poolConnections.query(`CALL public.get_person('result')`);
-        const users = await poolConnections.query(`FETCH ALL IN "result";`);
-        await poolConnections.query(`COMMIT;`);
-        return users.rows;
-    } catch(e) {
-        console.error('error', e);
-        return null;
-    }
-}
-
